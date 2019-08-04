988f441ceb1e0ce8a8b7a94e86752939f834b422
suqiang0313 <suqiang0313@gmail.com>
2019-08-04 22:35:26 +0800

    create Mat

--- example/003/main.cpp
+++ example/003/main.cpp
@@ -14,9 +14,22 @@ int main() {
     }
     namedWindow("input", CV_WINDOW_AUTOSIZE);
     imshow("input", src);
-    Mat gray;
-    cvtColor(src,gray,COLOR_BGR2GRAY);
-    imshow("gray color",gray);
+    Mat m1 = src.clone();
+    Mat m2;
+    src.copyTo(m2);
+    // m3 and src have same address
+    Mat m3 = src;
+    Mat m4 = Mat::zeros(src.size(),src.type());
+    cout << m4.size()<<endl;
+    // new Mat with zeros
+    Mat m5 = Mat::zeros(Size(512,512),CV_8UC3);
+    // new Mat with ones
+    Mat m6 = Mat::ones(Size(512,512),CV_8UC3);
+//    imshow("zeros",255);
+    // new kernel
+    Mat kernel = (Mat_<char>(3, 3) << 0, -1, 0,
+        -1, 5, -1,
+        0, -1, 0);
     waitKey(0);
     return 0;
 }
